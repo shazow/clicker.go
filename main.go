6ae1db48c6b90a36720a8e94f0de669e46299782
Andrey Petrov <andrey.petrov@shazow.net>
2017-01-30 17:00:45 -0500

    Simplify a lot.

--- main.go
+++ main.go
@@ -1,24 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gopherjs/vecty"
-	"github.com/shazow/clicker.go/components"
+	"github.com/gopherjs/vecty/elem"
+	"github.com/gopherjs/vecty/event"
 )
 
 func main() {
 	vecty.SetTitle("clicker.go")
-	g := components.NewGame()
 
-	reqCounter := components.NewCounterView("requests")
-	reqCounter.Button("Add Goroutine")
-	g.AddCounter(reqCounter)
+	count := 0.0
+	rate := 0.1
 
-	vecty.RenderBody(g)
+	v := &View{
+		OnClick: func(_ *vecty.Event) {
+			rate += 0.1
+		},
+	}
+	vecty.RenderBody(v)
 
 	ticker := time.Tick(50 * time.Millisecond)
 	for _ = range ticker {
-		g.Tick()
+		count += rate
+
+		v.Count = int(count)
+		vecty.Rerender(v)
 	}
+}
+
+type View struct {
+	vecty.Core
+
+	Count   int
+	OnClick func(event *vecty.Event)
+}
+
+func (v *View) Render() *vecty.HTML {
+	return elem.Body(
+		elem.Header1(
+			vecty.Text("clicker.go"),
+		),
+		elem.Div(
+			vecty.Text(fmt.Sprintf("%d %s", v.Count, "requests")),
+		),
+		elem.Div(
+			elem.Button(
+				vecty.Text("Add Goroutine"),
+				event.Click(v.OnClick),
+			),
+		),
+	)
 }
